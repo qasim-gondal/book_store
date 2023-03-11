bed574ef353f367ed9f642444c84b2b53c48803e
qasim-gondal <qgondal48@gmail.com>
2023-03-11 19:38:50 +0500

    modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do  
-  resources :books do 
-    member do
-put "add" , to: "books#library"
-put "remove" , to: "books#library"
-    end  
-  end
+  resources :books
  devise_for :users 
   resources :library
   root 'books#index'
